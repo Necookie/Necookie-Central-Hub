f79318915db9e4f01b90b3e95709b1226501327f
necookie <dheyn.main@gmail.com>
2026-01-27 23:39:48 +0800

    Initial commit: Setup PERN stack structure and Tailwind

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Necookie API Running'));
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server on port ${PORT}`));
